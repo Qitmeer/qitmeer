1bbcc1afd97120f565b9363c7ba8ae99811218d2
Alex Wu <dindinw@users.noreply.github.com>
2018-05-31 18:17:41 +0800

    add omni

---
+++ script/myomni/start.sh
@@ -0,0 +1,3 @@
+data_dir=/data/omnilayer/private
+
+./omnicored --datadir=$data_dir --txindex --reindex --server --regtest --printtoconsole
--- script/myqtum/start.sh
+++ script/myqtum/start.sh
@@ -1,3 +1,3 @@
 data_dir=/data/qtum/private
 
-./qtumd --datadir=$data_dir --txindex --reindex --logevents --server --regtest
+./qtumd --datadir=$data_dir --txindex --reindex --logevents -record-log-opcodes --server --regtest --printtoconsole
