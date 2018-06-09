33bad24faf63a1b6c1c7f7896e17ee0db9b80547
Alex Wu <dindinw@users.noreply.github.com>
2018-06-09 17:12:42 +0800

    testnet script

--- script/mybtc/start_testnet.sh
+++ script/mybtc/start_testnet.sh
@@ -1,5 +1,6 @@
 data_dir=/data/bitcoin/testnet
 
-# ./bitcoind -testnet  --datadir=$data_dir --txindex --server  --printtoconsole --debug=net
-# ./bitcoind -testnet  --datadir=$data_dir --txindex --printtoconsole
-./bitcoind -testnet --datadir=$data_dir --txindex 
+# ./bitcoind -testnet --datadir=$data_dir --txindex --server  --printtoconsole --debug=net
+# ./bitcoind -testnet --datadir=$data_dir --txindex --printtoconsole
+./bitcoind -testnet --datadir=$data_dir --txindex --printtoconsole --connect=0 --debug=net
+
