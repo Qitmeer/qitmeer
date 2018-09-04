4191088df3f05984e837433893473a7fa5fa4353
Alex Wu <dindinw@users.noreply.github.com>
2018-09-04 18:28:06 +0800

    start exmple script

---
+++ script/nox/start.sh
@@ -0,0 +1,11 @@
+
+network="--privnet"
+mining="--miningaddr RmFUBXUpN3W6bSgaBHpPQzQ7pXNb3fG59Kn"
+debug="-d trace --printorigin"
+rpc="--listen 127.0.0.1:1234 --rpcuser test --rpcpass test"
+path="-b ."
+index="--txindex"
+
+./nox $debug $rpc $path $index $network $mining "$@"
+
+
