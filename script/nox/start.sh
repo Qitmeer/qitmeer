656179c50041e85a498bbacb7127d430dc098599
FuLingjie <apeuu@gmail.com>
2018-09-15 19:01:13 +0800

    update start.sh

--- script/nox/start.sh
+++ script/nox/start.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 network="--privnet"
 mining="--miningaddr RmFUBXUpN3W6bSgaBHpPQzQ7pXNb3fG59Kn"
@@ -6,6 +7,6 @@ rpc="--listen 127.0.0.1:1234 --rpcuser test --rpcpass test"
 path="-b ."
 index="--txindex"
 
-./nox $debug $rpc $path $index $network $mining "$@"
+../../nox $debug $rpc $path $index $network $mining "$@"
 
 
