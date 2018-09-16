220b3089d884c2e895d383d6099240d193944c8d
Alex Wu <dindinw@users.noreply.github.com>
2018-09-16 12:30:29 +0800

    how to use dev script

--- script/nox/start.sh
+++ script/nox/start.sh
@@ -7,6 +7,17 @@ rpc="--listen 127.0.0.1:1234 --rpcuser test --rpcpass test"
 path="-b ."
 index="--txindex"
 
-../../nox $debug $rpc $path $index $network $mining "$@"
+#1.) The start script used only for dev test purpose
+#2.) the relative path of nox executable link sould be the same path of the start.sh link 
+#3.) the "-b ." set the base data directory as the same place where the start script executed
+#
+# EX:
+# $ WORK=/tmp/my_test_workspace
+# $ mkdir -p $WORK
+# $ cd $WORK
+# $ ls -s /where/is/my/nox/executable
+# $ ln -s /where/is/my/nox/start.sh
+# $ ./start.sh
+./nox $debug $rpc $path $index $network $mining "$@"
 
 
