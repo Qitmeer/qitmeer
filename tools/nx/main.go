b46ccbc0bf44d62690663e07a01e38e2180f25dd
Alex Wu <dindinw@users.noreply.github.com>
2018-09-19 12:14:44 +0800

    refactoring nx

--- tools/nx/main.go
+++ tools/nx/main.go
@@ -3,6 +3,53 @@
 // license that can be found in the LICENSE file.
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+const (
+	NX_VERSION = "0.0.1"
+)
+
+func init() {
+
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr,"Usage: nx [--version] [--help] <command> [<args>]\n")
+	fmt.Fprintf(os.Stderr,`
+Nox commmands :
+    base58check-decode    Decode base58check hex string from stdin
+`)
+	os.Exit(1)
+}
+
+func version() {
+	fmt.Fprintf(os.Stderr,"Nx Version : %q\n",NX_VERSION)
+	os.Exit(1)
+}
+
+func main() {
+	decodeCommand := flag.NewFlagSet("base58check-decode", flag.ExitOnError)
+	if len(os.Args) == 1 {
+		usage()
+	}
+	switch os.Args[1]{
+	case "help","--help" :
+		usage()
+	case "version","--version":
+		version()
+	case "base58check-decode" :
+		decodeCommand.Parse(os.Args[1:])
+	default:
+		invalid := os.Args[1]
+		if invalid[0] == '-' {
+			fmt.Fprintf(os.Stderr, "unknown option: %q \n", invalid)
+		}else {
+			fmt.Fprintf(os.Stderr, "%q is not valid command\n", invalid)
+		}
+		os.Exit(1)
+	}
 }
