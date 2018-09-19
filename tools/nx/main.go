df5464f93514b0ccdf901de7d89e094a95a95ff9
Alex Wu <dindinw@users.noreply.github.com>
2018-09-19 15:13:42 +0800

    nx base58encode & polishing

--- tools/nx/main.go
+++ tools/nx/main.go
@@ -10,21 +10,19 @@ import (
 	"github.com/noxproject/nox/common/encode/base58"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
 const (
 	NX_VERSION = "0.0.1"
 )
 
-func init() {
-
-}
-
 func usage() {
 	fmt.Fprintf(os.Stderr,"Usage: nx [--version] [--help] <command> [<args>]\n")
 	fmt.Fprintf(os.Stderr,`
 Nox commmands :
     base58check-encode    Encode base58check hex string from stdin
+    base58check-decode    Decode base58check hex string from stdin
 `)
 	os.Exit(1)
 }
@@ -34,12 +32,21 @@ func version() {
 	os.Exit(1)
 }
 
+func errExit(err error){
+	fmt.Fprintf(os.Stderr, "Nx Error : %q\n",err)
+	os.Exit(1)
+}
+
+var base58version string
+var showDecodeDetails bool
 
 
 func main() {
 	encodeCommand := flag.NewFlagSet("base58check-encode", flag.ExitOnError)
-	var base58version string
 	encodeCommand.StringVar(&base58version, "v","0df1","base58check version")
+
+	decodeCommand := flag.NewFlagSet("base58check-decode", flag.ExitOnError)
+	decodeCommand.BoolVar(&showDecodeDetails,"d",false, "show decode datails")
 
 	if len(os.Args) == 1 {
 		usage()
@@ -51,6 +58,8 @@ func main() {
 		version()
 	case "base58check-encode" :
 		encodeCommand.Parse(os.Args[2:])
+	case "base58check-decode" :
+		decodeCommand.Parse(os.Args[2:])
 	default:
 		invalid := os.Args[1]
 		if invalid[0] == '-' {
@@ -66,32 +75,53 @@ func main() {
 		if (stat.Mode() & os.ModeNamedPipe) == 0 {
 			switch os.Args[2] {
 			case "help","--help":
-				fmt.Fprintf(os.Stderr, "Usage: nx base58check-encode [hexstring]\n")
+				fmt.Fprintf(os.Stderr, "Usage: nx base58check-encode [-v <ver>] [hexstring]\n")
 				encodeCommand.PrintDefaults()
 			default:
-				noxBase58CheckEncode(base58version,os.Args[len(os.Args)-1])
+				base58CheckEncode(base58version,os.Args[len(os.Args)-1])
 			}
 		}else {  //try from STDIN
 			src, err := ioutil.ReadAll(os.Stdin)
 			if err != nil {
-				panic(err)
+				errExit(err)
 			}
-			noxBase58CheckEncode(base58version,string(src))
+			str := strings.TrimSpace(string(src))
+			base58CheckEncode(base58version,str)
+		}
+	}
+
+	if decodeCommand.Parsed(){
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeNamedPipe) == 0 {
+			switch os.Args[2] {
+			case "help","--help":
+				fmt.Fprintf(os.Stderr, "Usage: nx base58check-decode [-d] [hexstring]\n")
+				decodeCommand.PrintDefaults()
+			default:
+				base58CheckDecode(os.Args[len(os.Args)-1])
+			}
+		}else {  //try from STDIN
+			src, err := ioutil.ReadAll(os.Stdin)
+			if err != nil {
+				errExit(err)
+			}
+			str := strings.TrimSpace(string(src))
+			base58CheckDecode(str)
 		}
 	}
 }
 
-func noxBase58CheckEncode(version string, input string){
+func base58CheckEncode(version string, input string){
 	data, err := hex.DecodeString(input)
 	if err!=nil {
-		panic(err)
+		errExit(err)
 	}
 	ver, err := hex.DecodeString(version)
 	if err !=nil {
-		panic(err)
+		errExit(err)
 	}
 	if len(ver) != 2 {
-		panic("invaid version byte")
+		errExit(fmt.Errorf("invaid version byte"))
 	}
 	var versionByte [2]byte
 	versionByte[0] = ver[0]
@@ -100,4 +130,18 @@ func noxBase58CheckEncode(version string, input string){
 	// Show the encoded data.
 	//fmt.Printf("Encoded Data ver[%v] : %s\n",ver, encoded)
 	fmt.Printf("%s\n",encoded)
+}
+
+func base58CheckDecode(input string) {
+	data, ver, err := base58.CheckDecode(input)
+	if err != nil {
+		errExit(err)
+	}
+	if showDecodeDetails {
+		fmt.Printf("payload : %x\n", data)
+		fmt.Printf("checksum: %x\n", data)
+		fmt.Printf("version : %x\n", ver)
+	}else {
+		fmt.Printf("%x\n", data)
+	}
 }
