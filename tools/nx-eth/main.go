f71b40ea0dc367c185de36a7c4012cf2fe834852
Alex Wu <dindinw@users.noreply.github.com>
2018-10-06 21:05:07 +0800

    nox/nx : clean up && remove unnecessary dependencies

--- tools/nx-eth/main.go
+++ tools/nx-eth/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"github.com/ethereum/go-ethereum/crypto"
-	"fmt"
-	"os"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"encoding/hex"
-	"github.com/ethereum/go-ethereum/common/math"
+	"errors"
+	"fmt"
+	"github.com/noxproject/nox/common/hash"
+	"github.com/noxproject/nox/common/math"
+	"github.com/noxproject/nox/crypto/ecc/secp256k1"
+	"math/big"
+	"os"
 )
 
 // Lengths of hashes and addresses in bytes.
@@ -38,7 +41,7 @@ func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
 	if pub == nil || pub.X == nil || pub.Y == nil {
 		return nil
 	}
-	return elliptic.Marshal(crypto.S256(), pub.X, pub.Y)
+	return elliptic.Marshal(secp256k1.S256(), pub.X, pub.Y)
 }
 
 // FromECDSAPrv exports a private key into a binary dump.
@@ -51,7 +54,7 @@ func FromECDSAPriv(priv *ecdsa.PrivateKey) []byte {
 
 func PubkeyToAddress(p ecdsa.PublicKey) Address {
 	pubBytes := FromECDSAPub(&p)
-	return BytesToAddress(crypto.Keccak256(pubBytes[1:])[12:])
+	return BytesToAddress(keccak256(pubBytes[1:])[12:])
 }
 
 func main() {
@@ -59,7 +62,7 @@ func main() {
 	args := os.Args[1:]
 	inputPrviKey := args[0]
 
-	privkey, err := crypto.HexToECDSA(inputPrviKey)
+	privkey, err := hexToECDSA(inputPrviKey)
 	if err != nil {
 		panic(err)
 	}
@@ -70,9 +73,9 @@ func main() {
 	fmt.Printf("priv %s\n",hex.EncodeToString(FromECDSAPriv(privkey)))
 	fmt.Printf("pub %s \n", hex.EncodeToString(FromECDSAPub(&pubKey)))
 
-	pubkeyHash0 := crypto.Keccak256(FromECDSAPub(&pubKey))
-	pubkeyHash1 := crypto.Keccak256(FromECDSAPub(&pubKey)[1:])
-	pubkeyHash40 := crypto.Keccak256(FromECDSAPub(&pubKey)[1:])[12:]
+	pubkeyHash0 := keccak256(FromECDSAPub(&pubKey))
+	pubkeyHash1 := keccak256(FromECDSAPub(&pubKey)[1:])
+	pubkeyHash40 := keccak256(FromECDSAPub(&pubKey)[1:])[12:]
 
 	fmt.Printf("pkhash %s \n", hex.EncodeToString(pubkeyHash0))
 	fmt.Printf("pkhash %s \n", hex.EncodeToString(pubkeyHash1))
@@ -81,4 +84,43 @@ func main() {
 	addr := PubkeyToAddress(pubKey)
 	fmt.Printf("add %v \n", addr)
 	fmt.Printf("add %s \n", hex.EncodeToString(addr[:]))
+}
+
+func keccak256(data []byte) []byte {
+	return hash.CalcHash(data,hash.GetHasher(hash.Keccak_256))
+}
+
+func hexToECDSA(hexkey string) (*ecdsa.PrivateKey, error) {
+	b, err := hex.DecodeString(hexkey)
+	if err != nil {
+		return nil, errors.New("invalid hex string")
+	}
+	return toECDSA(b,true)
+}
+
+// toECDSA creates a private key with the given D value. The strict parameter
+// controls whether the key's length should be enforced at the curve size or
+// it can also accept legacy encodings (0 prefixes).
+func toECDSA(d []byte, strict bool) (*ecdsa.PrivateKey, error) {
+	priv := new(ecdsa.PrivateKey)
+	priv.PublicKey.Curve = secp256k1.S256()
+	if strict && 8*len(d) != priv.Params().BitSize {
+		return nil, fmt.Errorf("invalid length, need %d bits", priv.Params().BitSize)
+	}
+	priv.D = new(big.Int).SetBytes(d)
+
+	// The priv.D must < N
+	if priv.D.Cmp(secp256k1.S256().N) >= 0 {
+		return nil, fmt.Errorf("invalid private key, >=N")
+	}
+	// The priv.D must not be zero or negative.
+	if priv.D.Sign() <= 0 {
+		return nil, fmt.Errorf("invalid private key, zero or negative")
+	}
+
+	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
+	if priv.PublicKey.X == nil {
+		return nil, errors.New("invalid private key")
+	}
+	return priv, nil
 }
