3935b26985be687187f4fd03c3bd564d5c68d08f
Alex Wu <dindinw@users.noreply.github.com>
2018-10-08 13:50:27 +0800

    nx : base58check-decode imporve cksum display

--- util/bytes.go
+++ util/bytes.go
@@ -11,3 +11,10 @@ func CopyBytes(b []byte) (copiedBytes []byte) {
 
 	return
 }
+
+func ReverseBytes(b []byte){
+	for i := len(b)/2-1; i >= 0; i-- {
+		opp := len(b)-1-i
+		b[i], b[opp] = b[opp], b[i]
+	}
+}
