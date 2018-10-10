a6951f473643cb15fcb86f96c9945dc4273ab6be
Alex Wu <dindinw@users.noreply.github.com>
2018-10-10 13:07:43 +0800

    nx : fix code format

--- tools/nx/nox58checkflag.go
+++ tools/nx/nox58checkflag.go
@@ -18,7 +18,7 @@ func (n *noxBase58checkVersionFlag) Set(s string) error {
 	case "mainnet":
 		n.ver = append(n.ver, params.MainNetParams.PubKeyHashAddrID[0:]...)
 	case "privnet":
-	    n.ver = append(n.ver, params.PrivNetParams.PubKeyHashAddrID[0:]...)
+		n.ver = append(n.ver, params.PrivNetParams.PubKeyHashAddrID[0:]...)
 	case "testnet":
 		n.ver = append(n.ver, params.TestNetParams.PubKeyHashAddrID[0:]...)
 	default:
