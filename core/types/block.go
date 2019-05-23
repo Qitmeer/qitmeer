ab82a6a9352a99025db908a68717ba680aa17d78
Jin <lochjin@gmail.com>
2019-05-23 17:16:00 +0800

    Notes about MaxBlockPayload

--- core/types/block.go
+++ core/types/block.go
@@ -19,8 +19,7 @@ import (
 const MaxBlockHeaderPayload = 4 + (hash.HashSize * 3) + 4 + 8 + 8 +8
 
 // MaxBlockPayload is the maximum bytes a block message can be in bytes.
-// After Segregated Witness, the max block payload has been raised to 1048576 = 1024*1024.
-const MaxBlockPayload = 1048576
+const MaxBlockPayload = 1048576 // 1024*1024 (1MB)
 
 // maxTxPerBlock is the maximum number of transactions that could
 // possibly fit into a block.
