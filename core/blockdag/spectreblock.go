bb8707bf2ee0f08b9293f93f8d694eaaf56781b1
Jin <lochjin@gmail.com>
2019-06-13 18:17:04 +0800

    Optimize spectreblock

---
+++ core/blockdag/spectreblock.go
@@ -0,0 +1,34 @@
+package blockdag
+
+import "github.com/HalalChain/qitmeer/common/hash"
+
+type ISpectre interface {
+	Vote(x IBlock, y IBlock) int
+}
+
+type SpectreBlock struct {
+	hash hash.Hash
+	Votes1, Votes2 int // votes in future set, -1 means not voted yet
+}
+
+func (sb *SpectreBlock) GetHash() *hash.Hash {
+	return &sb.hash
+}
+
+type SpectreBlockData struct {
+	hash hash.Hash
+	parents []*hash.Hash
+	timestamp int64
+}
+
+func (sd *SpectreBlockData) GetHash() *hash.Hash {
+	return &sd.hash
+}
+
+func (sd *SpectreBlockData) GetParents() []*hash.Hash {
+	return sd.parents
+}
+
+func (sd *SpectreBlockData) GetTimestamp() int64 {
+	return sd.timestamp
+}
