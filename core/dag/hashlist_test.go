1cda9ec3f1b30735699eeb99820cf636157629e4
Jin <lochjin@gmail.com>
2019-07-01 18:26:50 +0800

    Migrate to qx

---
+++ core/dag/hashlist_test.go
@@ -0,0 +1,15 @@
+package dag
+
+import (
+	"github.com/HalalChain/qitmeer-lib/common/hash"
+	"testing"
+)
+
+func Test_Has(t *testing.T) {
+	hl:=HashList{}
+	hl=append(hl,&hash.ZeroHash)
+
+	if !hl.Has(&hash.ZeroHash) {
+		t.FailNow()
+	}
+}
--- core/types/tx.go
+++ core/types/tx.go
@@ -3,6 +3,7 @@
 package types
 
 import (
+	"github.com/HalalChain/qitmeer-lib/common/math"
 	"io"
 	"github.com/HalalChain/qitmeer-lib/common/hash"
 	s "github.com/HalalChain/qitmeer-lib/core/serialization"
@@ -920,6 +921,28 @@ func writeTxInWitness(w io.Writer, pver uint32, ti *TxInput) error {
 
 	// Write the signature script.
 	return s.WriteVarBytes(w, pver, ti.SignScript)
+}
+
+// IsCoinBaseTx determines whether or not a transaction is a coinbase.  A
+// coinbase is a special transaction created by miners that has no inputs.
+// This is represented in the block chain by a transaction with a single input
+// that has a previous output transaction index set to the maximum value along
+// with a zero hash.
+//
+// This function only differs from IsCoinBase in that it works with a raw wire
+// transaction as opposed to a higher level util transaction.
+func (tx *Transaction) IsCoinBaseTx() bool {
+	// A coin base must only have one transaction input.
+	if len(tx.TxIn) != 1 {
+		return false
+	}
+	// The previous output of a coin base must have a max value index and a
+	// zero hash.
+	prevOut := &tx.TxIn[0].PreviousOut
+	if prevOut.OutIndex != math.MaxUint32 || !prevOut.Hash.IsEqual(&hash.ZeroHash) {
+		return false
+	}
+	return true
 }
 
 // Tx defines a transaction that provides easier and more efficient manipulation
