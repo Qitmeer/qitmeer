22a913047a494e3870516e7ea8b5269260f4fac1
Jin <lochjin@gmail.com>
2019-07-04 17:26:37 +0800

    Add RPC:GetPeerInfo

--- core/json/node.go
+++ core/json/node.go
@@ -11,3 +11,33 @@ type InfoNodeResult struct {
 	TestNet         bool    `json:"testnet"`
 	Errors          string  `json:"errors"`
 }
+
+// GetPeerInfoResult models the data returned from the getpeerinfo command.
+type GetPeerInfoResult struct {
+	ID             int32   `json:"id"`
+	Addr           string  `json:"addr"`
+	AddrLocal      string  `json:"addrlocal,omitempty"`
+	Services       string  `json:"services"`
+	RelayTxes      bool    `json:"relaytxes"`
+	LastSend       int64   `json:"lastsend"`
+	LastRecv       int64   `json:"lastrecv"`
+	BytesSent      uint64  `json:"bytessent"`
+	BytesRecv      uint64  `json:"bytesrecv"`
+	ConnTime       int64   `json:"conntime"`
+	TimeOffset     int64   `json:"timeoffset"`
+	PingTime       float64 `json:"pingtime"`
+	PingWait       float64 `json:"pingwait,omitempty"`
+	Version        uint32  `json:"version"`
+	SubVer         string  `json:"subver"`
+	Inbound        bool    `json:"inbound"`
+	BanScore       int32   `json:"banscore"`
+	SyncNode       bool    `json:"syncnode"`
+	GraphState     GetGraphStateResult `json:"graphstate"`
+}
+
+// GetGraphStateResult data
+type GetGraphStateResult struct {
+	Tips []string `json:"tips"`
+	Total uint32 `json:"total"`
+	Layer uint32 `json:"layer"`
+}
