d3248f44e49baa70130b53ebe45b7b071202f882
Jin <lochjin@gmail.com>
2019-07-26 16:53:09 +0800

    Repair log file

--- log.go
+++ log.go
@@ -23,26 +23,59 @@ import (
 var (
 	glogger *log.GlogHandler
 
+	logWrite *logWriter
+)
+
+// logWriter implements an io.Writer that outputs to both standard output and
+// the write-end pipe of an initialized log rotator.
+type logWriter struct{
 	// logRotator is one of the logging outputs.  It should be closed on
 	// application shutdown.
 	logRotator *rotator.Rotator
-)
 
+	// Use for color terminal
+	colorableWrite io.Writer
+}
 
+func (lw *logWriter) Init() {
+	// init a colorful logger if possible
+	usecolor := term.IsTty(os.Stdout.Fd()) && os.Getenv("TERM") != "dumb"
+
+	if usecolor {
+		lw.colorableWrite = colorable.NewColorableStderr()
+	}
+}
+
+func (lw *logWriter) Close() {
+	if lw.logRotator != nil {
+		lw.logRotator.Close()
+	}
+}
+
+func (lw *logWriter) IsUseColor() bool {
+	return lw.colorableWrite != nil
+}
+
+func (lw *logWriter) Write(p []byte) (n int, err error) {
+	os.Stderr.Write(p)
+	if lw.logRotator != nil {
+		lw.logRotator.Write(p)
+	}
+
+	if lw.colorableWrite != nil {
+		lw.colorableWrite.Write(p)
+	}
+
+	return len(p), nil
+}
 
 func init() {
-
-	// init a colorful logger if possible
-	usecolor := term.IsTty(os.Stderr.Fd()) && os.Getenv("TERM") != "dumb"
-
 	// output set to Stderr
 	// it's easier to handle when run as a daemon through systemd or supervisord,
 	// and Go runtime exceptions are printed to stderr as well.
-	output := io.Writer(os.Stderr)
-	if usecolor {
-		output = colorable.NewColorableStderr()
-	}
-	glogger = log.NewGlogHandler(log.StreamHandler(output, log.TerminalFormat(usecolor)))
+	logWrite=&logWriter{}
+	logWrite.Init()
+	glogger = log.NewGlogHandler(log.StreamHandler(io.Writer(logWrite), log.TerminalFormat(logWrite.IsUseColor())))
 
 	log.Root().SetHandler(glogger)
 
@@ -75,5 +108,5 @@ func initLogRotator(logFile string) {
 		os.Exit(1)
 	}
 
-	logRotator = r
+	logWrite.logRotator = r
 }
