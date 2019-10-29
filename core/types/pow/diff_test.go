68c77663c099ac3f628ba01187b3906ac6090299
james <james.van2019@outlook.com>
2019-10-29 17:39:12 +0800

    fix test

--- core/types/pow/diff_test.go
+++ core/types/pow/diff_test.go
@@ -70,9 +70,14 @@ func TestCalcNextDiff(t *testing.T) {
 	p := &PowConfig{
 		Blake2bdPowLimit:     new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 232), common.Big1),
 		Blake2bdPowLimitBits: 0x1e00ffff,
-		Blake2bDPercent:      34,
-		CuckarooPercent:      33,
-		CuckatooPercent:      33,
+		Percent:[]Percent{
+			{
+				Blake2bDPercent: 34,
+				CuckarooPercent: 33,
+				CuckatooPercent: 33,
+				MainHeight:      0,
+			},
+		},
 		//hash ffffffffffffffff000000000000000000000000000000000000000000000000 corresponding difficulty is 48 for edge bits 24
 		// Uniform field type uint64 value is 48 . bigToCompact the uint32 value
 		// 24 edge_bits only need hash 1*4 times use for privnet if GPS is 2. need 50 /2 * 2 ≈ 1min find once
@@ -82,6 +87,8 @@ func TestCalcNextDiff(t *testing.T) {
 	blakeObj := &Blake2bd{}
 	oldDiff := int64(10000)
 	oldDiffBig := big.NewInt(oldDiff)
+	blakeObj.SetMainHeight(1)
+	blakeObj.SetParams(p)
 	// actual time 2s  target time 5s
 	// current pow count 4 all count 100
 	weightBig := big.NewInt(2)
@@ -90,7 +97,7 @@ func TestCalcNextDiff(t *testing.T) {
 	currentPowPercent := big.NewInt(4)
 	currentPowPercent.Lsh(currentPowPercent, 32)
 	currentPowPercent.Div(currentPowPercent, big.NewInt(100))
-	nextDiffBig := blakeObj.GetNextDiffBig(weightBig, oldDiffBig, currentPowPercent, p)
+	nextDiffBig := blakeObj.GetNextDiffBig(weightBig, oldDiffBig, currentPowPercent)
 	//10000 * ( 2 / 5 ) / (4 / 34)
 	assert.Equal(t, uint64(10000*2*34/5/4), nextDiffBig.Uint64())
 }
@@ -101,15 +108,21 @@ func TestCalcCuckarooNextDiff(t *testing.T) {
 	p := &PowConfig{
 		Blake2bdPowLimit:     new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 232), common.Big1),
 		Blake2bdPowLimitBits: 0x1e00ffff,
-		Blake2bDPercent:      34,
-		CuckarooPercent:      33,
-		CuckatooPercent:      33,
+		Percent:[]Percent{
+			{
+				Blake2bDPercent: 34,
+				CuckarooPercent: 33,
+				CuckatooPercent: 33,
+				MainHeight:      0,
+			},
+		},
 		//hash ffffffffffffffff000000000000000000000000000000000000000000000000 corresponding difficulty is 48 for edge bits 24
 		// Uniform field type uint64 value is 48 . bigToCompact the uint32 value
 		// 24 edge_bits only need hash 1*4 times use for privnet if GPS is 2. need 50 /2 * 2 ≈ 1min find once
 		CuckarooMinDifficulty: 0x1300000 * 2,
 		CuckatooMinDifficulty: 0x1300000 * 2,
 	}
+
 	oldDiff := int64(10000)
 	oldDiffBig := big.NewInt(oldDiff)
 	// actual time 2s  target time 5s
@@ -119,12 +132,14 @@ func TestCalcCuckarooNextDiff(t *testing.T) {
 	weightBig.Div(weightBig, big.NewInt(5))
 	//cuckaroo diff ajustment
 	cuckarooObj := &Cuckaroo{}
+	cuckarooObj.SetMainHeight(1)
+	cuckarooObj.SetParams(p)
 	// actual time 2s  target time 5s
 	// current pow count 4 all count 100
 	currentPowPercent := big.NewInt(4)
 	currentPowPercent.Lsh(currentPowPercent, 32)
 	currentPowPercent.Div(currentPowPercent, big.NewInt(100))
-	nextDiffBig := cuckarooObj.GetNextDiffBig(weightBig, oldDiffBig, currentPowPercent, p)
+	nextDiffBig := cuckarooObj.GetNextDiffBig(weightBig, oldDiffBig, currentPowPercent)
 	//10000 / ( 2 / 5 ) * (4 / 33)
 	assert.Equal(t, uint64(10000*5*4/2/33), nextDiffBig.Uint64())
 }
@@ -135,9 +150,14 @@ func TestCalcCuckatooNextDiff(t *testing.T) {
 	p := &PowConfig{
 		Blake2bdPowLimit:     new(big.Int).Sub(new(big.Int).Lsh(common.Big1, 232), common.Big1),
 		Blake2bdPowLimitBits: 0x1e00ffff,
-		Blake2bDPercent:      34,
-		CuckarooPercent:      33,
-		CuckatooPercent:      33,
+		Percent:[]Percent{
+			{
+				Blake2bDPercent: 34,
+				CuckarooPercent: 33,
+				CuckatooPercent: 33,
+				MainHeight:      0,
+			},
+		},
 		//hash ffffffffffffffff000000000000000000000000000000000000000000000000 corresponding difficulty is 48 for edge bits 24
 		// Uniform field type uint64 value is 48 . bigToCompact the uint32 value
 		// 24 edge_bits only need hash 1*4 times use for privnet if GPS is 2. need 50 /2 * 2 ≈ 1min find once
@@ -152,13 +172,15 @@ func TestCalcCuckatooNextDiff(t *testing.T) {
 	weightBig.Lsh(weightBig, 32)
 	weightBig.Div(weightBig, big.NewInt(5))
 	//cuckaroo diff ajustment
-	cuckarooObj := &Cuckatoo{}
+	cuckatooObj := &Cuckatoo{}
+	cuckatooObj.SetMainHeight(1)
+	cuckatooObj.SetParams(p)
 	// actual time 2s  target time 5s
 	// current pow count 4 all count 100
 	currentPowPercent := big.NewInt(4)
 	currentPowPercent.Lsh(currentPowPercent, 32)
 	currentPowPercent.Div(currentPowPercent, big.NewInt(100))
-	nextDiffBig := cuckarooObj.GetNextDiffBig(weightBig, oldDiffBig, currentPowPercent, p)
+	nextDiffBig := cuckatooObj.GetNextDiffBig(weightBig, oldDiffBig, currentPowPercent)
 	//10000 / ( 2 / 5 ) * (4 / 33)
 	assert.Equal(t, uint64(10000*5*4/2/33), nextDiffBig.Uint64())
 }
