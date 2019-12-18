d5dc7ea5d408e2fef24b4a639c081eba787d7496
forchain <tony.goldenweek@gmail.com>
2019-12-19 02:22:03 +0800

    update anticone equation

--- core/blockdag/anticone/anticone.go
+++ core/blockdag/anticone/anticone.go
@@ -14,64 +14,32 @@ var myLog = log.New(os.Stdout, "", 0)
 //
 // delay: Max propagation delay, unit is second
 // rate: Block rate, unit is blocks/second
-// level: Security level, the probability of an honest block being marked red
+// security: Security level, the probability of an honest block being marked red
 func GetSize(_delay, _rate, _security float64) int {
-
-	factor := 2 * _delay * _rate
-	if factor > 10000 {
-		myLog.Fatalf("keep factor:%v = 2 * _delay:%v * _rate:%v under 1000!", factor, _delay, _rate)
+	expect := 2 * _delay * _rate
+	if expect > 999 {
+		myLog.Fatalf("keep expect:%v = 2 * _delay:%v * _rate:%v under 1000!", expect, _delay, _rate)
 	}
 
-	coef := math.Pow(math.E, factor)
+	coef := 1 / math.Pow(math.E, expect)
 
-	//myLog.Printf("_delay:%v _rate:%v  level:%v factor:%v coef:%v\n\n", _delay, _rate, _security, factor, coef)
-
-	sum := 0.0
-
-	outLen := 10
-	kQueue := make([]float64, 0)
+	myLog.Printf("_delay:%v _rate:%v  _security:%v expect:%v coef:%v\n\n", _delay, _rate, _security, expect, coef)
 
 	end := 1000
 
-	k := -1
+	sum := 1.0
 
-	for kk := 1; kk < end; kk++ {
-		sum = coef
-
-		sigma := 1.0
-		for j := 1; j <= kk; j++ {
-			n := 1.0
-			for jj := 1; jj <= j; jj++ {
-				n *= factor / float64(jj)
-			}
-			sigma += n
+	for k := 0; k < end; k++ {
+		part := 1.0
+		for j := 1; j <= k; j++ {
+			part *= expect / float64(j)
 		}
-		sum -= sigma
-		sum /= coef
-
-		if k < 0 {
-			if sum < _security {
-				for i := 0; i < len(kQueue); i++ {
-					leftBound := outLen
-					if kk <= leftBound {
-						leftBound = kk - 1
-					}
-					//myLog.Printf("kk=%v sum=%v", kk-(leftBound-i), kQueue[i])
-				}
-				//myLog.Printf("\n[MIN]kk=%v sum=%v\n\n", kk, sum)
-				k = kk
-				end = kk + outLen + 1
-			}
-
-			kQueue = append(kQueue, sum)
-			if kk > outLen {
-				kQueue = kQueue[1:]
-			}
-
-		} else {
-			//myLog.Printf("kk=%v sum=%v", kk, sum)
+		sum -= part * coef
+		myLog.Printf("k=%v sum=%v", k, sum)
+		if sum < _security {
+			return k
 		}
 	}
+	return 0
 
-	return k
 }
