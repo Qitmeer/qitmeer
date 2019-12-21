4e3471274ca9646829441d4b5b504bf8ccfc13fd
Jin <lochjin@gmail.com>
2019-12-21 14:46:09 +0800

    update version

--- core/protocol/protocol.go
+++ core/protocol/protocol.go
@@ -13,10 +13,10 @@ import (
 const (
 	// InitialProcotolVersion is the initial protocol version for the
 	// network.
-	InitialProcotolVersion uint32 = 18
+	InitialProcotolVersion uint32 = 19
 
 	// ProtocolVersion is the latest protocol version this package supports.
-	ProtocolVersion uint32 = 18
+	ProtocolVersion uint32 = 19
 )
 
 // Network represents which qitmeer network a message belongs to.
