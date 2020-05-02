377bace9e0e3ff866367d4906ee40034172c4ca1
jamesvan2019 <james.van2019@outlook.com>
2020-05-02 21:24:30 +0800

    update comment about version

--- common/version.go
+++ common/version.go
@@ -1,17 +1,14 @@
 // Copyright (c) 2017-2019 The Qitmeer developers
 //
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
-// The parts code inspired & originated from
-// http://github.com/ethereum/go-ethereum/common/size.go
-
 package common
 
 import (
 	"encoding/binary"
 )
 
+//To maintain version
+//use the first 2 bytes for version
+//last 2 bytes use for rand nonce
 func GetQitmeerBlockVersion(blockVersion uint32) uint32 {
 	b := make([]byte, 4)
 	newVersionData := make([]byte, 4)
