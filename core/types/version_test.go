20b22e2ceb19fc20ee3f55b1c2353cb141d19e60
jamesvan2019 <james.van2019@outlook.com>
2020-05-04 10:48:52 +0800

    version

--- core/types/version_test.go
+++ core/types/version_test.go
@@ -2,16 +2,13 @@ package types
 
 import (
 	"encoding/binary"
-	"github.com/Qitmeer/qitmeer/common"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 var (
-	versionData   = []byte{18, 0, 0, 0}   // uint32 => 18
-	version1Data  = []byte{18, 0, 17, 28} // uint32 => 470876178
-	originVersion = uint32(18)
-	version2      = uint32(470876178)
+	versionData  = []byte{18, 0, 0, 0}   // uint32 => 18
+	version1Data = []byte{18, 0, 17, 28} // uint32 => 470876178
 )
 
 func TestVersion(t *testing.T) {
@@ -19,6 +16,8 @@ func TestVersion(t *testing.T) {
 	version1 := binary.LittleEndian.Uint32(version1Data)
 	assert.NotEqual(t, version1, version)
 	assert.Equal(t, version1Data[:2], versionData[:2])
-	newVersion := common.GetQitmeerBlockVersion(version2)
-	assert.Equal(t, originVersion, newVersion)
+	header := &BlockHeader{
+		Version: version1,
+	}
+	assert.Equal(t, version, header.GetVersion())
 }
