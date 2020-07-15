f8f55f88475bcc21c76983e4749a22b10e099057
Jin <lochjin@gmail.com>
2020-07-15 09:39:49 +0800

    Optimize p2p config

--- p2p/config.go
+++ p2p/config.go
@@ -5,25 +5,28 @@ import "os"
 // Config for the p2p service.
 // to initialize the p2p service.
 type Config struct {
-	NoDiscovery         bool
-	EnableUPnP          bool
-	DisableDiscv5       bool
-	StaticPeers         []string
-	BootstrapNodeAddr   []string
-	Discv5BootStrapAddr []string
-	RelayNodeAddr       string
-	LocalIP             string
-	HostAddress         string
-	HostDNS             string
-	PrivateKey          string
-	DataDir             string
-	MetaDataDir         string
-	TCPPort             uint
-	UDPPort             uint
-	MaxPeers            uint
-	AllowListCIDR       string
-	DenyListCIDR        []string
-	Encoding            string
-
+	NoDiscovery          bool
+	EnableUPnP           bool
+	StaticPeers          []string
+	BootstrapNodeAddr    []string
+	Discv5BootStrapAddr  []string
+	DataDir              string
+	MaxPeers             uint
+	MetaDataDir          string
 	ReadWritePermissions os.FileMode
+	AllowListCIDR        string
+	DenyListCIDR         []string
+	TCPPort              uint
+	UDPPort              uint
+	// EnableNoise enables the beacon node to use NOISE instead of SECIO when performing a handshake with another peer.
+	EnableNoise   bool
+	RelayNodeAddr string
+
+	//TODO
+
+	LocalIP     string
+	HostAddress string
+	HostDNS     string
+	PrivateKey  string
+	Encoding    string
 }
