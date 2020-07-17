8051b189e5d126f3b87be9955260439f575483d4
Jin <lochjin@gmail.com>
2020-07-17 11:18:45 +0800

    deadlines

---
+++ p2p/deadlines.go
@@ -0,0 +1,35 @@
+package p2p
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/libp2p/go-libp2p-core/network"
+)
+
+var defaultReadDuration = ttfbTimeout
+var defaultWriteDuration = 10 * time.Second
+
+// SetRPCStreamDeadlines sets read and write deadlines for libp2p-based connection streams.
+func SetRPCStreamDeadlines(stream network.Stream) {
+	SetStreamReadDeadline(stream, defaultReadDuration)
+	SetStreamWriteDeadline(stream, defaultWriteDuration)
+}
+
+// SetStreamReadDeadline for reading from libp2p connection streams, deciding when to close
+// a connection based on a particular duration.
+func SetStreamReadDeadline(stream network.Stream, duration time.Duration) {
+	if err := stream.SetReadDeadline(time.Now().Add(duration)); err != nil {
+		log.Debug(fmt.Sprintf("Failed to set stream deadline:%v peer:%s protocol:%s direction:%s",
+			err, stream.Conn().RemotePeer(), stream.Protocol(), stream.Stat().Direction))
+	}
+}
+
+// SetStreamWriteDeadline for writing to libp2p connection streams, deciding when to close
+// a connection based on a particular duration.
+func SetStreamWriteDeadline(stream network.Stream, duration time.Duration) {
+	if err := stream.SetWriteDeadline(time.Now().Add(duration)); err != nil {
+		log.Debug(fmt.Sprintf("Failed to set stream deadline:%v peer:%s protocol:%s direction:%s",
+			err, stream.Conn().RemotePeer(), stream.Protocol(), stream.Stat().Direction))
+	}
+}
