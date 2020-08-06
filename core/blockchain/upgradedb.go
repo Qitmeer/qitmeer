68e2e1cbdb3ffa2ce3e52114c6f70bdac9b76823
Jin <lochjin@gmail.com>
2020-08-06 17:36:28 +0800

    [Fix #343]Upgrade the database for compatibility with older versions

--- core/blockchain/upgradedb.go
+++ core/blockchain/upgradedb.go
@@ -2,7 +2,9 @@ package blockchain
 
 import (
 	"fmt"
+	"github.com/Qitmeer/qitmeer/common/hash"
 	"github.com/Qitmeer/qitmeer/core/dbnamespace"
+	"github.com/Qitmeer/qitmeer/core/types"
 	"github.com/Qitmeer/qitmeer/database"
 	"time"
 )
@@ -12,24 +14,19 @@ func (b *BlockChain) upgradeDB() error {
 	if b.dbInfo.version == currentDatabaseVersion {
 		return nil
 	}
-	log.Info(fmt.Sprintf("Update cur db to new version: version(%d) -> version(%d)", b.dbInfo.version, currentDatabaseVersion))
+	log.Info(fmt.Sprintf("Update cur db to new version: version(%d) -> version(%d) ...", b.dbInfo.version, currentDatabaseVersion))
 	err := b.db.Update(func(dbTx database.Tx) error {
 		meta := dbTx.Metadata()
 		bidxStart := time.Now()
-
-		serializedData := meta.Get(dbnamespace.ChainStateKeyName)
-		if serializedData == nil {
+		spendBucket := meta.Bucket(dbnamespace.SpendJournalBucketName)
+		if spendBucket == nil {
 			return nil
 		}
-
-		state, err := deserializeBestChainState(serializedData)
+		err := checkSpendJournal(dbTx, spendBucket)
 		if err != nil {
 			return err
 		}
-		err = b.bd.UpgradeDB(dbTx, uint(state.total))
-		if err != nil {
-			return err
-		}
+		// save
 		b.dbInfo = &databaseInfo{
 			version: currentDatabaseVersion,
 			compVer: currentCompressionVersion,
@@ -44,5 +41,39 @@ func (b *BlockChain) upgradeDB() error {
 		log.Info(fmt.Sprintf("Update db version:time=%v", time.Since(bidxStart)))
 		return nil
 	})
+	if err != nil {
+		return fmt.Errorf("You can cleanup your block data base by '--cleanup'.The data is corrupted and cannot be upgraded (%s). ", err)
+	}
 	return err
+}
+
+func checkSpendJournal(dbTx database.Tx, spendBucket database.Bucket) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+	var blockHash *hash.Hash
+	var block *types.SerializedBlock
+	cursor := spendBucket.Cursor()
+	for ok := cursor.First(); ok; ok = cursor.Next() {
+		serialized := spendBucket.Get(cursor.Key())
+		if len(serialized) <= 0 {
+			continue
+		}
+
+		blockHash, err = hash.NewHash(cursor.Key())
+		if err != nil {
+			return err
+		}
+		block, err = dbFetchBlockByHash(dbTx, blockHash)
+		if err != nil {
+			return err
+		}
+		_, err = dbFetchSpendJournalEntry(dbTx, block)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
