bc718d740b9a517ae7c656aa5946b5897263d91e
bCoder778 <bCoder778@gmail.com>
2020-11-17 17:22:30 +0800

    Add p2p crawler cmd

---
+++ cmd/crawler/crawler.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/Qitmeer/qitmeer/cmd/crawler/log"
+	"github.com/Qitmeer/qitmeer/cmd/crawler/node"
+	"github.com/Qitmeer/qitmeer/common/roughtime"
+	_ "github.com/Qitmeer/qitmeer/database/ffldb"
+	_ "github.com/Qitmeer/qitmeer/services/common"
+	"github.com/urfave/cli/v2"
+	"os"
+)
+
+func main() {
+	if err := crawlerNode(); err != nil {
+		log.Log.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func crawlerNode() error {
+	n := &node.Node{}
+	app := &cli.App{
+		Name:     "CrawlerNode",
+		Version:  "V0.0.1",
+		Compiled: roughtime.Now(),
+		Authors: []*cli.Author{
+			&cli.Author{
+				Name: "Qitmeer",
+			},
+		},
+		Copyright:            "(c) 2020 Qitmeer",
+		Usage:                "Crawler Node",
+		Flags:                node.AppFlags,
+		EnableBashCompletion: true,
+		Before: func(c *cli.Context) error {
+			return n.Init(node.Conf)
+		},
+		After: func(c *cli.Context) error {
+			return n.Exit()
+		},
+		Action: func(c *cli.Context) error {
+			return n.Run()
+		},
+	}
+
+	return app.Run(os.Args)
+}
