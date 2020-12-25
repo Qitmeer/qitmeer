791bd014a2913f2347bfcc75c679b6b43b9a8502
Alex Wu <dindinw@users.noreply.github.com>
2020-12-25 17:04:46 +0800

    add rpc client and test

---
+++ testutils/testservice.go
@@ -0,0 +1,22 @@
+package testutils
+
+import "strings"
+
+type TestService struct{}
+
+type Complex struct {
+	X, Y int
+	Name string
+}
+
+type EchoResult struct {
+	String string
+	Int    int
+	Complex   *Complex
+}
+
+func (s *TestService) Echo(str string, i int, comp *Complex) EchoResult {
+	comp.Name = strings.ToUpper(comp.Name)
+	return EchoResult{strings.ToUpper(str), i, comp}
+}
+
