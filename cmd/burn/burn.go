0c9377eb511d55811a9d769cbff997257c61189d
Alex Wu <dindinw@users.noreply.github.com>
2021-07-18 10:33:36 +0800

    burn: improve the default template for multiple networks

--- cmd/burn/burn.go
+++ cmd/burn/burn.go
@@ -16,12 +16,14 @@ import (
 
 const addrSize int = 35
 const alphabet string = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+const defaultTemplate string = "TmQitmeerTestNetBurnAddress"
+const defaultNetwork string = "testnet"
 var template string
 var network string
 
 func init() {
-	flag.StringVar(&template, "t","TmQitmeerTestNetBurnAddress" ,"template")
-	flag.StringVar(&network, "n","testnet" ,"network [mainnet|testnet|mixnet|privnet]")
+	flag.StringVar(&template, "t", defaultTemplate,"template")
+	flag.StringVar(&network, "n",defaultNetwork ,"network [mainnet|testnet|mixnet|privnet]")
 	rand.Seed(time.Now().UnixNano())
 }
 
@@ -36,6 +38,14 @@ func main() {
 	flag.Parse()
 	p, err := getParams(network);
 	exitIfErr(err)
+	if network != defaultNetwork && template == defaultTemplate {
+		var sb strings.Builder
+		sb.WriteString(p.NetworkAddressPrefix)
+		sb.WriteString("mQitmeer")
+		sb.WriteString(strings.Title(p.Name))
+		sb.WriteString("BurnAddress")
+		template = sb.String();
+	}
 	addr, err := getAddr(template,p)
 	exitIfErr(err)
 	fmt.Printf("template = %s \n", template)
