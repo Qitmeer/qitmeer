23913dfedaea95bd2e13b073dc7289b5bfdcd5e4
Alex Wu <dindinw@users.noreply.github.com>
2021-07-18 10:18:38 +0800

    cmd : add burn addr gen tool

---
+++ cmd/burn/burn_test.go
@@ -0,0 +1,35 @@
+// Copyright 2021 The qitmeer developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"fmt"
+	"github.com/Qitmeer/qitmeer/params"
+	"strings"
+	"testing"
+)
+
+func TestBurnAddrForNetworks(t *testing.T) {
+	testNetworkPrefix("Mm", &params.MainNetParams, t)
+	testNetworkPrefix("Tm", &params.TestNetParams, t)
+	testNetworkPrefix("Xm", &params.MixNetParams, t)
+	testNetworkPrefix("Rm", &params.PrivNetParams, t)
+}
+
+func testNetworkPrefix(prefix string , p *params.Params, t *testing.T) {
+	var sb strings.Builder
+	sb.WriteString(prefix)
+	sb.WriteString("Qitmeer")
+	sb.WriteString(strings.Title(p.Name))
+	sb.WriteString("BurnAddress")
+	prefixed := sb.String()
+	addr, err := getAddr(prefixed, p)
+	if err != nil {
+		t.Fail();
+	}
+	if !strings.HasPrefix(string(addr),prefixed) {
+		t.Fail();
+	}
+	fmt.Printf("%s burn addr: %s tested ok!\n",p.Name, addr)
+}
