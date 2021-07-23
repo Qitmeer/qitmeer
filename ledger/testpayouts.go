a6341be8f1454cb6cfe5adcab80bb70fe07bec15
james <james@james.com>
2021-07-23 10:10:24 +0800

    add test genesis addr

--- ledger/testpayouts.go
+++ ledger/testpayouts.go
@@ -19,6 +19,12 @@ var TestGeneData = []GenesisInitPayout{
 	{
 		types.MEERID, "TmYxDYgDc8H3xMYHU2gbnvg7NN6gFpTDYia", 100000, GENE_PAYOUT_TYPE_STANDARD, 0,
 	},
+	{
+		types.MEERID, "TmYFQGAXtF3KeBG962kimzMtcTQyj6njrcA", 100000, GENE_PAYOUT_TYPE_STANDARD, 0,
+	},
+	{
+		types.MEERID, "TmjWgubD5FcPTcbWLyvwEkZ2s9aCDVqPJmL", 100000, GENE_PAYOUT_TYPE_STANDARD, 0,
+	},
 }
 
 // coinid,address,lockAmount,locktype,lockheight
@@ -26,6 +32,7 @@ var TestGeneDataFromImport = []string{
 	"0,XmLbv8fvAvrBFtQu2e6PwAxqrkMnuwW4Wbg,50000,1,0",
 	"0,XmU8ZwQBcWz4WNo9mxnFrfrzd9L9UjShGD9,33000,1,0",
 	"0,XmLGBNVDwgsQQLYVkMghZhhggSsXiUuY2SK,50000,1,0",
+	"0,TmYnGyFdNsvMGeq4quKWnvCtxfFs372Kh3t,50000,1,0",
 }
 
 func main() {
